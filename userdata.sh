13745c09571afb8d69042d743e96983e85e233d5
EC2 Default User <ec2-user@ip-172-31-18-142.ec2.internal>
2023-03-14 19:46:36 +0000

    should define your key file

---
+++ userdata.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+hostnamectl set-hostname ${server-name}
+yum update -y
+amazon-linux-extras install docker -y
+systemctl start docker
+systemctl enable docker
+usermod -a -G docker ec2-user
